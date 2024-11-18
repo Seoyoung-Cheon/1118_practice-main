3f872109d9623b791c65b894a94b2369521728c1
unknown <gu05087@gmail.com>
2024-11-18 21:57:54 +0900

    1118_h

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Mainsection from "./components/Mainsection";
+import Topbar from "./components/Topbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Topbar />
+      <Mainsection />
+    </div>
+  );
+};
+
+export default App;
